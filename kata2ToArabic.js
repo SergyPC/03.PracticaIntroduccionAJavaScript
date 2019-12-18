4f8b03eeede6d268d4577f0e74005be36e623af1
Sergio Perez <sergy.perez@gmail.com>
2019-12-18 17:02:56 +0100

    Cambio el nombre de una variable

--- kata2ToArabic.js
+++ kata2ToArabic.js
@@ -1,14 +1,14 @@
 const toArabic = number => {
   const roman = number.toString().toUpperCase().split('');
   const search = {I:1,IV:4,V:5,IX:9,X:10,XL:40,L:50,XC:90,C:100,CD:400,D:500,CM:900,M:1000};
-  let numberArabic = 0;
+  let arabicNumber = 0;
   for (let i = ((roman.length)-1); i >= 0; i--) {
     if ( search[roman[i]] < search[roman[i+1]] )
-      numberArabic -= search[roman[i]];
+      arabicNumber -= search[roman[i]];
     else
-      numberArabic += search[roman[i]];
+      arabicNumber += search[roman[i]];
   }
-  return numberArabic;
+  return arabicNumber;
 }
 
 const number = process.argv.splice(2).toString().toUpperCase();
