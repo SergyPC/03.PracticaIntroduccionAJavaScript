0b4b26401a64a4691f0df5d18a9a9b7247d4b311
Sergio Perez <sergy.perez@gmail.com>
2019-12-18 13:32:45 +0100

    Creo el fichero kata2ToArabic.js

---
+++ kata2ToArabic.js
@@ -0,0 +1,19 @@
+const toArabic = number => {
+  const roman = number.toString().toUpperCase().split('');
+  const search = {I:1,IV:4,V:5,IX:9,X:10,XL:40,L:50,XC:90,C:100,CD:400,D:500,CM:900,M:1000};
+  let numberArabic = 0;
+  for (let i = ((roman.length)-1); i >= 0; i--) {
+    if ( search[roman[i]] < search[roman[i+1]] )
+      numberArabic -= search[roman[i]];
+    else
+      numberArabic += search[roman[i]];
+  }
+  return numberArabic;
+}
+
+const number = process.argv.splice(2).toString().toUpperCase();
+
+if (!(/^M*(?:V?I{0,3}|I[XV])(?:L?X{0,3}|X[CL])(?:D?C{0,3}|C[MD])$/.test(number)))
+  console.log(`The Roman number ${number} isn't a valid number.`);
+else
+  console.log(`The Roman number ${number} is ${toArabic(number)}.`);
