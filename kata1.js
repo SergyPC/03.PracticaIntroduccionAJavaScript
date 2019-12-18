72c8a4c4e60a8897125388584981192761103610
Sergio Perez <sergy.perez@gmail.com>
2019-12-18 13:24:33 +0100

    Creo el fichero Kata1.js

---
+++ kata1.js
@@ -0,0 +1,55 @@
+function fooBarQuix (number) {
+    let response = '';
+    let array = number.toString().split('');
+
+    if(number % 3 === 0) response += 'Foo';
+    if(number % 5 === 0) response += 'Bar';
+    if(number % 7 === 0) response += 'Quix';
+
+    array.forEach(function(character) {
+        switch (character) {
+            case '3':
+                response += 'Foo';
+                break;
+            case '5':
+                response += 'Bar';
+                break;
+            case '7':
+                response += 'Quix';
+                break;
+        }
+    });
+
+    if (response === '') response = number;
+    
+    return response;
+}
+
+
+const end = process.argv.splice(2);
+
+if ( (!isNaN(end)) && end > 0 && end < 101 ) {
+    for (let index = 1; index <= end; index++) {
+        const spaces = (index < 10) ? '  ' : (index > 9 && index < 100) ? ' ' : '';
+        console.log(`* ${index}${spaces}-> ${fooBarQuix(index)}`);
+        //const zeros = (index < 10) ? '00' : (index > 9 && index < 100) ? '0' : '';
+        //console.log(`* ${zeros}${index} -> ${fooBarQuix(index)}`);
+    }
+} 
+else 
+    console.log('Introduzca un número entre 1 y 100');
+
+
+/*
+const start = process.argv.splice(2);
+if ( (!isNaN(start)) && start > 0 && start < 101 ) {
+    for (let index = start; index < 101; index++) {
+        const spaces = (index < 10) ? '  ' : (index > 9 && index < 100) ? ' ' : '';
+        console.log(`* ${index}${spaces}-> ${fooBarQuix(index)}`);
+        //const zeros = (index < 10) ? '00' : (index > 9 && index < 100) ? '0' : '';
+        //console.log(`* ${zeros}${index} -> ${fooBarQuix(index)}`);
+    }
+} 
+else 
+    console.log('Introduzca un número entre 1 y 100');
+*/
